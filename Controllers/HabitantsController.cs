3bd9ae010047b130408ccf9fcade94a5ca5d4208
nvnemtin <nxn@inbox.ru>
2021-02-15 18:15:59 -0600

    UULAPI-1: Inhabitant deletion was implemented

--- Controllers/HabitantsController.cs
+++ Controllers/HabitantsController.cs
@@ -61,5 +61,31 @@ namespace uul_api.Controllers {
             }
             return response;
         }
+
+
+        [HttpPost("delete")]
+        [Authorize]
+        public async Task<ActionResult<UULResponse>> DeleteHabitant(HabitantDTO habitantDTO) {
+            var currentUser = HttpContext.User;
+            UULResponse response;
+            try {
+                var userInfo = SecHelper.GetUserInfo(currentUser.Claims);
+                var user = await _context.Users.Where(u => u.Login.Equals(userInfo.Login) && u.ApartmentCode.Equals(userInfo.ApartmentCode)).FirstAsync();
+                var existentHabitants = await _context.Habitants.Where(h => h.User.ID == user.ID).Select(h => new HabitantDTO(h)).ToListAsync();
+                if (existentHabitants.Count <= 1) {
+                    return new UULResponse() { Success = false, Message = "Can not delete the last inhabitant", Data = null };
+                }
+                var habitant = await _context.Habitants.FindAsync(habitantDTO.ID);
+                _context.Habitants.Remove(habitant);
+                await _context.SaveChangesAsync();
+                var habitants = await _context.Habitants.Where(h => h.User.ID == user.ID).Select(h => new HabitantDTO(h)).ToListAsync();
+                userInfo.IsActivated = user.IsActivated;
+                userInfo.Habitants = habitants;
+                response = new UULResponse() { Success = true, Message = "Habitant was deleted", Data = userInfo };
+            } catch (Exception e) {
+                response = new UULResponse() { Success = false, Message = e.Message, Data = null };
+            }
+            return response;
+        }
     }
 }
